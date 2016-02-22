566dd4ad5ef433d085e465699419491135bd138e
mk-jhpiego <makando.kabila@jhpiego.org>
2016-02-22 18:17:23 +0200

    Added logic to accomodate sites ending in 1

---
+++ JhpiegoDb/Views/FacilityLookupAll.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[FacilityLookupAll]
+AS
+SELECT FacilityLookUp.FacilityIndex, Left([FacilityId],6) AS FacilityHmisCode
+FROM FacilityLookUp 
+UNION SELECT FacilityLookUp.FacilityIndex, Left([FacilityId],6)+'0' AS FacilityHmisCode
+FROM FacilityLookUp
+UNION SELECT FacilityLookUp.FacilityIndex, Left([FacilityId],6)+'1' AS FacilityHmisCode
+FROM FacilityLookUp;
